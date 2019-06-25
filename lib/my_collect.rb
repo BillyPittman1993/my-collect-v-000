c2cca63dfb9b0d82751e190a204c11ced853db05
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-25 13:48:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 def my_collect(collection)
   i = 0 
   collection_new = []
   while i < collection.length
   collection_new << yield(collection[i])
   i += 1 
+  binding.pry
   end
   collection_new
 end
