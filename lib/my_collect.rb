28ace4c67c0e528c9c8a9ec6a6378779c9a1e535
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-25 13:47:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0 
   collection_new = []
   while i < collection.length
-  collection_new << yield(array[i])
+  collection_new << yield(collection[i])
   i += 1 
   end
   collection_new
